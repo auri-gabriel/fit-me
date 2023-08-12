07f51a2380f0349a5b8df3a8fd9bdf732376d89e
Auri Gabriel <ririmello.a@gmail.com>
2023-08-11 21:10:28 -0300

    feat: cards list layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import FoodCard from '../components/Cards/FoodCard';
 
-const HomeContainer = styled.div`
+const RestaurantsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
-  margin: 20px;
+  align-items: center;
+  justify-content: flex-start;
   gap: 1.5rem;
+  margin: 1rem 0;
+
+  @media (max-width: 1024px){
+    justify-content: center;
+  }
+`;
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 18rem;
+  
+  h2 {
+    font-size: 24px;
+  }
+
+  @media (max-width: 1440px){
+    padding: 0 10rem;
+  }
+
+  @media (max-width: 1024px){
+    padding: 0;
+    h2 {
+      text-align: center;
+    }
+  }
 `;
 
 const Home: React.FC = () => {
@@ -26,20 +51,65 @@ const Home: React.FC = () => {
       region: 'Italy',
       rating: 4.5
     },
+    {
+      title: 'asdfk;jlasd;ljkl;kjs ad;ljkfl;jkdasf;ljksdf',
+      description: 'Cheesy and mouthwatering pizza.',
+      imageUrl: 'src/assets/placeholder.png',
+      region: 'Italy',
+      rating: 4.5
+    },
+    {
+      title: 'asdfk;jlasd;ljkl;kjs ad;ljkfl;jkdasf;ljksdf',
+      description: 'Cheesy and mouthwatering pizza.',
+      imageUrl: 'src/assets/placeholder.png',
+      region: 'Italy',
+      rating: 4.5
+    },
+    {
+      title: 'asdfk;jlasd;ljkl;kjs ad;ljkfl;jkdasf;ljksdf',
+      description: 'Cheesy and mouthwatering pizza.',
+      imageUrl: 'src/assets/placeholder.png',
+      region: 'Italy',
+      rating: 4.5
+    },
+    {
+      title: 'asdfk;jlasd;ljkl;kjs ad;ljkfl;jkdasf;ljksdf',
+      description: 'Cheesy and mouthwatering pizza.',
+      imageUrl: 'src/assets/placeholder.png',
+      region: 'Italy',
+      rating: 4.5
+    },
+    {
+      title: 'asdfk;jlasd;ljkl;kjs ad;ljkfl;jkdasf;ljksdf',
+      description: 'Cheesy and mouthwatering pizza.',
+      imageUrl: 'src/assets/placeholder.png',
+      region: 'Italy',
+      rating: 4.5
+    },
+    {
+      title: 'asdfk;jlasd;ljkl;kjs ad;ljkfl;jkdasf;ljksdf',
+      description: 'Cheesy and mouthwatering pizza.',
+      imageUrl: 'src/assets/placeholder.png',
+      region: 'Italy',
+      rating: 4.5
+    },
   ];
 
   return (
     <HomeContainer>
-      {foodItems.map((item, index) => (
-        <FoodCard
-          key={index}
-          title={item.title}
-          imageUrl={item.imageUrl}
-          region={item.region}
-          rating={item.rating}
-          arrivalTime='30 min'
-        />
-      ))}
+      <h2>Restaurants</h2>
+      <RestaurantsContainer>
+        {foodItems.map((item, index) => (
+          <FoodCard
+            key={index}
+            title={item.title}
+            imageUrl={item.imageUrl}
+            region={item.region}
+            rating={item.rating}
+            arrivalTime='30 min'
+          />
+        ))}
+      </RestaurantsContainer>
     </HomeContainer>
   );
 };
