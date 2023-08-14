8d009d0a071a50f68a05bffdfa4f3005df38c742
Auri Gabriel <ririmello.a@gmail.com>
2023-08-14 15:13:32 -0300

    feat: validation

--- src/components/Forms/Textfield.tsx
+++ src/components/Forms/Textfield.tsx
@@ -1,16 +1,23 @@
-import React, { ChangeEvent } from 'react';
+import React, {ChangeEvent} from 'react';
 import styled from 'styled-components';
 
 const TextFieldContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  div {
+    margin-top: 0.5rem;
+  }
 `;
 
 const Label = styled.label`
   font-size: 1.25rem;
   margin-bottom: 1rem;
 `;
+
+interface InputProps {
+  error?: boolean;
+}
 
 const Input = styled.input`
   font-size: 1.25rem;
@@ -19,6 +26,7 @@ const Input = styled.input`
   width: 100%;
   border-radius: 10px;
   box-sizing: border-box;
+  border-color: ${({ error }) => (error ? 'red' : '#FC8019')};
 `;
 
 interface TextFieldProps {
@@ -26,14 +34,16 @@ interface TextFieldProps {
   type?: string;
   name?: string;
   value?: string;
-  onChange?: (event: ChangeEvent<HTMLInputElement>) => void; // Add onChange handler
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  error?: string | null;
 }
 
-const TextField: React.FC<TextFieldProps> = ({ label, type = 'text', name, value, onChange }) => {
+const TextField: React.FC<TextFieldProps> = ({label, type = 'text', name, value, onChange, error}) => {
   return (
     <TextFieldContainer>
       <Label>{label}</Label>
-      <Input type={type} name={name} value={value} onChange={onChange} />
+      <Input type={type} name={name} value={value} onChange={onChange} error={!!error}/>
+      {error && <div style={{color: 'red'}}>{error}</div>}
     </TextFieldContainer>
   );
 };
