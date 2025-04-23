909ece4c0d1c9b888044e3fd7e8c213a961ff702
Auri Gabriel <ririmello.a@gmail.com>
2025-04-23 16:38:16 -0300

    feat: standardize quotation marks in Navbar component for consistency

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,25 +7,31 @@ import burgerIcon from '/src/assets/Burger.svg';
 
 const HomeNavbar: React.FC = () => {
   return (
-    <nav className="navbar-custom px-xl-5 px-lg-4 px-md-3 py-5 bg-white">
-      <div className="container d-flex justify-content-between align-items-center">
-        <Link to="/">
-          <img src={logo} alt="Logo" height="40" />
+    <nav className='navbar-custom px-xl-5 px-lg-4 px-md-3 py-5 bg-white'>
+      <div className='container d-flex justify-content-between align-items-center'>
+        <Link to='/'>
+          <img src={logo} alt='Logo' height='40' />
         </Link>
 
-        <div className="d-none d-md-flex align-items-center gap-4">
+        <div className='d-none d-md-flex align-items-center gap-4'>
           <input
-            type="text"
-            className="form-control search-bar py-3"
-            placeholder="Enter item or restaurant you are looking for"
+            type='text'
+            className='form-control search-bar py-3'
+            style={{ width: '450px' }}
+            placeholder='Enter item or restaurant you are looking for'
           />
-          <img src={bagIcon} alt="Bag" />
-          <button className="btn btn-secondary px-4 py-3 rounded-3">
-            <Link to="/login" className="fs-sm fw-bold text-white text-decoration-none">Sign&nbsp;In</Link>
+          <img src={bagIcon} alt='Bag' />
+          <button className='btn btn-secondary px-4 py-3 rounded-3'>
+            <Link
+              to='/login'
+              className='fs-sm fw-bold text-white text-decoration-none'
+            >
+              Sign&nbsp;In
+            </Link>
           </button>
         </div>
 
-        <img src={burgerIcon} alt="Menu" className="d-md-none" />
+        <img src={burgerIcon} alt='Menu' className='d-md-none' />
       </div>
     </nav>
   );
@@ -33,9 +39,9 @@ const HomeNavbar: React.FC = () => {
 
 const DefaultNavbar: React.FC = () => {
   return (
-    <nav className="navbar-custom px-xl-5 px-lg-4 px-md-3 py-5 bg-white">
-      <div className="container-fluid d-flex justify-content-between align-items-center">
-        <img src={logo} alt="Logo" height="40" />
+    <nav className='navbar-custom px-xl-5 px-lg-4 px-md-3 py-5 bg-white'>
+      <div className='container-fluid d-flex justify-content-between align-items-center'>
+        <img src={logo} alt='Logo' height='40' />
       </div>
     </nav>
   );
