8c8e3d5d19d2bd4d26ec06bf7471bea4d7026d88
Auri Gabriel <ririmello.a@gmail.com>
2025-04-24 12:41:33 -0300

    refactor: replace styled-components with Bootstrap grid system in ItemBody for improved layout

--- src/components/pages/Item/ItemBody.tsx
+++ src/components/pages/Item/ItemBody.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import ItemCard from './ItemCard';
 
 interface Dish {
@@ -21,54 +20,20 @@ interface ItemBodyProps {
   dishes: Dish[] | TopDishes[];
 }
 
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-  padding: 1rem 20rem;
-
-  @media (max-width: 1440px){
-    padding: 5rem 10rem;
-  }
-
-  @media (max-width: 1024px){
-    padding: 5rem 5rem;
-    flex-direction: column;
-  }
-`;
-
-const FiltersColumn = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const ItemsColumn = styled.div`
-  flex: 2;
-  padding: 20px;
-`;
-
-const CartColumn = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
 const ItemBody: React.FC<ItemBodyProps> = ({ dishes }) => {
   return (
-    <Container>
-      <FiltersColumn>
-        Top Dishes
-      </FiltersColumn>
-      <ItemsColumn>
-        {dishes.map((dish) => (
-          <ItemCard dish={dish} />
-        ))}
-      </ItemsColumn>
-      <CartColumn>
-        Cart
-      </CartColumn>
-    </Container>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-lg-2'>Top Dishes</div>
+        <div className='col-lg-7'>
+          {dishes.map((dish) => (
+            <ItemCard dish={dish} />
+          ))}
+        </div>
+        <div className='col-lg-3'>Cart</div>
+      </div>
+    </div>
   );
 };
 
 export default ItemBody;
-
