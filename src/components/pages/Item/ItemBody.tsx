1fe92700e2e65fe07a8d16b0064572bcdf1a5712
Auri Gabriel <ririmello.a@gmail.com>
2025-04-25 16:34:47 -0300

    refactor: implement category filtering for dishes in ItemBody component

--- src/components/pages/Item/ItemBody.tsx
+++ src/components/pages/Item/ItemBody.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ItemCard from './ItemCard';
 
 interface Dish {
@@ -7,6 +7,7 @@ interface Dish {
   image: string;
   description: string;
   price: number;
+  category?: string;
 }
 
 interface ItemBodyProps {
@@ -14,15 +15,59 @@ interface ItemBodyProps {
 }
 
 const ItemBody: React.FC<ItemBodyProps> = ({ dishes }) => {
+  // Mocked categories
+  const categories = [
+    'Recommended',
+    'Breakfast Box',
+    'Lunch Box',
+    'Combo Box',
+    'Biriyani Box',
+  ];
+
+  const [selectedCategory, setSelectedCategory] =
+    useState<string>('Recommended');
+  const [dishesWithCategories, setDishesWithCategories] = useState<Dish[]>([]);
+
+  // Assign random categories to dishes
+  useEffect(() => {
+    const dishesWithRandomCategories = dishes.map((dish) => ({
+      ...dish,
+      category: categories[Math.floor(Math.random() * categories.length)],
+    }));
+    setDishesWithCategories(dishesWithRandomCategories);
+  }, [dishes]);
+
+  const filteredDishes = dishesWithCategories.filter(
+    (dish) => dish.category === selectedCategory
+  );
+
   return (
     <div className='container'>
-      <div className='row'>
-        <div className='col-12 col-lg-2'>Top Dishes</div>
+      <div className='row my-5'>
+        <div className='col-12 col-lg-2 border-end'>
+          <ul className='nav flex-column'>
+            {categories.map((category) => (
+              <li className='nav-item' key={category}>
+                <button
+                  className={`nav-link fw-bold ${
+                    selectedCategory === category
+                      ? 'text-primary'
+                      : 'text-secondary'
+                  }`}
+                  onClick={() => setSelectedCategory(category)}
+                >
+                  {category}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
         <div className='col-12 col-lg-7'>
-          {dishes.map((dish) => (
-            <ItemCard dish={dish} />
+          {filteredDishes.map((dish) => (
+            <ItemCard key={dish.id} dish={dish} />
           ))}
         </div>
+
         <div className='d-none d-lg-flex col-lg-3'>Cart</div>
       </div>
     </div>
